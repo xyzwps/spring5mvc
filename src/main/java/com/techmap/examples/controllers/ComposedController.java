3fede9daa4315ae3e20bce0ce4ab7cb1b65cafbb
Angrynut <Angrynut@DESKTOP-DD0PQCF>
2016-09-07 08:51:59 +0800

    Composed @RequestMapping

--- src/main/java/com/techmap/examples/controllers/ComposedController.java
+++ src/main/java/com/techmap/examples/controllers/ComposedController.java
@@ -1,9 +1,56 @@
 package com.techmap.examples.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 组合的 @RequestMapping。
+ * 
+ * @author Angrynut
+ *
+ */
 @Controller
+@RequestMapping("/composed")
 public class ComposedController
 {
+    
+    @GetMapping("/get")
+    public String get()
+    {
+        return "/examples/targets/test1";
+    }
+    
+    /**
+     * 带有 URI 模板
+     */
+    @GetMapping(path = "/{day}")
+    public String getForDay(@PathVariable @DateTimeFormat(iso = ISO.DATE) Date day, Model model)
+    {
+        System.out.println("--> " + new SimpleDateFormat("yyyy-MM-dd").format(day));
+        
+        return "/examples/targets/test2";
+    }
+    
+    @PostMapping("/post")
+    public String post(
+            @RequestParam(value="username") String user, 
+            @RequestParam(value="password") String pass
+            )
+    {
+        System.out.println("--> Username: " + user);
+        System.out.println("--> Password: " + pass);
+        
+        return "/examples/targets/test3";
+    }
     
 }
--- src/main/webapp/WEB-INF/jsp/examples/helloWorld.jsp
+++ src/main/webapp/WEB-INF/jsp/examples/helloWorld.jsp
@@ -11,9 +11,26 @@
 </head>
 <body>
 	<h2>Hello World!</h2>
+	<hr>
 	<h2>Test Controller</h2>
-	<a href="contex">Test1</a><br>
-	<a href="contex/new">Test2</a><br>
-	<a href="contex/2016-09-05">Test3</a><br>
+	<a href="contex">Test1</a>
+	<br>
+	<a href="contex/new">Test2</a>
+	<br>
+	<a href="contex/2016-09-05">Test3</a>
+	<br>
+
+	<hr>
+	<h2>Composed RequestMapping</h2>
+	<a href="composed/get">Test1</a>
+	<br>
+	<a href="composed/2016-09-05">Test2</a>
+	<br>
+	<!-- 一个以 POST 方式提交的表单 -->
+	<form action="composed/post" method="post">
+		Username:<input type="text" name="username" placeholder="用户名..."><br>
+		Password:<input type="password" name="password" placeholder="用户名..."><br>
+		<button type="submit">Sign In</button>
+	</form>
 </body>
 </html>
